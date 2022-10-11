26392bd223580b4c6e93ece1d4b91487ac3ee77b
y.bikmaev@yandex.ru <y.bikmaev@yandex.ru>
2022-10-11 15:31:37 +0400

    Написана 5 программа

--- Lesson2/task_5.py
+++ Lesson2/task_5.py
@@ -1,1 +1,17 @@
-# Реализуйте алгоритм перемещения списка
+# Реализуйте алгоритм перемешивания списка
+
+from random import randint, shuffle
+
+# первый вариант
+lst = [i for i in range(10)]
+for i in range(len(lst)):
+    temp = lst[i]
+    r = randint(0, len(lst)-1)
+    lst[i] = lst[r]
+    lst[r] = temp
+print(lst)
+
+# второй вариант
+lst = [i for i in range(10)]
+shuffle(lst)
+print(lst)
