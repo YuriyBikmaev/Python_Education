770319d06df7b5aac22f86b3337c30d4a0119f0c
y.bikmaev@yandex.ru <y.bikmaev@yandex.ru>
2022-10-13 17:53:22 +0400

    внесеная информация по работе с requests, но пока не работает

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,5 +1,7 @@
 import json
+import requests
 from bs4 import BeautifulSoup
+
 
 datafile = open('Python_Education\Parser\webinars.json', 'r', encoding='utf-8')
 data = json.load(datafile)
@@ -12,4 +14,12 @@ htmlfile= open('Python_Education\Parser\skillbox.html', 'r', encoding='utf-8')
 code = htmlfile.read()
 soup = BeautifulSoup(code, "html.parser")
 links = soup.findAll('a')
-print(*[link.string for link in links])
+#print(*[link.string.strip() for link in links]) #вывод меню
+print(*[link['href'] for link in links]) #вывод ссылок
+
+kuf = requests.get("https://kuf.aero/board/?type=arr")
+print(kuf.status_code)
+#board__text-extra
+kufaero = BeautifulSoup(kuf.content, "html.parser")
+kuf_iata = kufaero.findAll('board__text-extra')
+print(*kuf_iata)
