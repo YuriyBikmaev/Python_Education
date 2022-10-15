081122e262e8cba55ed2ee289d8a7a95cf51cd35
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-10-15 10:13:23 +0400

    исправления

--- Lesson2/task_4.py
+++ Lesson2/task_4.py
@@ -1,11 +1,11 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число
 
-number_n = 4
+number_n = 3
 result = 1
 lst = list(range(-number_n, number_n+1))
 print(lst)
 data = open('file.txt', 'r')
-for el in data.readlines():
+for el in data:
     result *= lst[int(el)-1]
 print(result)
