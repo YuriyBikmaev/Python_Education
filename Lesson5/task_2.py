41dac8642153f8ed91bf26f5c5478aba585ce93e
y.bikmaev@yandex.ru <y.bikmaev@yandex.ru>
2022-10-28 17:30:10 +0400

    продолжена программа 2

--- Lesson5/task_2.py
+++ Lesson5/task_2.py
@@ -1,34 +1,57 @@
 # Создайте программу для игры с конфетами человек против человека.
-# Условие задачи: На столе лежит 2021 конфета. 
+# Условие задачи: На столе лежит 2021 конфета.
 # Играют два игрока делая ход друг после друга.
-# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. 
-# Все конфеты оппонента достаются сделавшему последний ход. 
-# Сколько конфет нужно взять первому игроку, 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку,
 # чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
+from random import choice
 
-def step_game(count):
+
+def step_game(count, start_player, list_player, block):
+    count -= take_candy(list_player[start_player], block)
+    start_player == 0 if start_player==1 else 1
     print(f"Осталось\n{count}")
 
-def take_candy(name):
-    return int(input(f"Игрок_{name} возьмет конфет:"))
+
+def take_candy(name, block):
+    try:
+        while True:
+            res = int(input(f"Игрок_{name} возьмет конфет:"))
+            if res<=block and res>=0:
+                return res
+            else:
+                print(f'{name}, Вы можете взять не больше {block} конфет')
+    except:
+        print('Введите колличество конфет')
+
 
 candy = 101
-player = ['Игрок_1', 'Игрок_1']
-progress = [0, 0]
+block_take = 28
+print('На столе лежит 2021 конфета.' +
+      '\n Играют два игрока делая ход друг после друга.'
+      '\n Первый ход определяется жеребьёвкой.'
+      '\n За один ход можно забрать не более чем 28 конфет.'
+      '\n Все конфеты оппонента достаются сделавшему последний ход.')
+starting_player = choice([0, 1])
+players = [input(f'Введите имя {i+1} игрока:\n') for i in range(2)]
+print(f'Игру начинает {players[starting_player]}')
 while True:
-    
-    take = take_candy(player[0])
-    candy -= take
-    progress[0] += take
-    step_game(candy)
-    take = take_candy(player[1])
-    candy -= take
-    progress[1] += take
-    step_game(candy)
-    if candy == 0:
+    step_game(candy, starting_player, players, block_take)
+    if candy==0:
+        print('Игра окончена')
         break
-
-print(progress)
+        
+#     take = take_candy(player[0])
+#     candy -= take
+#     progress[0] += take
+#     step_game(candy)
+#     take = take_candy(player[1])
+#     candy -= take
+#     progress[1] += take
+#     step_game(candy)
+#     if candy == 0:
+#         break
