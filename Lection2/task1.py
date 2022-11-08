3ae19842a10ef6d9d536f92c9a0321fcd77f3c56
y.bikmaev@yandex.ru <y.bikmaev@yandex.ru>
2022-11-08 17:12:09 +0400

    Доработан парсер

--- Lection2/task1.py
+++ Lection2/task1.py
@@ -1,7 +1,10 @@
 # num = '23,5'
 # print([*filter(lambda c: c not in ['.',','], num)])
 
-if not 2%2:
-    print('Четное')
-else:
-    print('Нечетное')
+lst = [4,2,11,7]
+number = 9
+for i in range(len(lst)):
+    z = number-lst[i]
+    if z in lst[i+1:]:
+        print([i, lst[i+1:].index(z)+i])
+        break
--- Parser/parser.py
+++ Parser/parser.py
@@ -3,23 +3,46 @@ import requests
 from bs4 import BeautifulSoup
 
 
-datafile = open('Python_Education\Parser\webinars.json', 'r', encoding='utf-8')
-data = json.load(datafile)
-#[webinar["speaker"] for webinar in data]
-#print([webinar["speaker"] for webinar in data])
-datafile.close
+# datafile = open('Python_Education\Parser\webinars.json', 'r', encoding='utf-8')
+# data = json.load(datafile)
+# #[webinar["speaker"] for webinar in data]
+# #print([webinar["speaker"] for webinar in data])
+# datafile.close
 
 
-htmlfile= open('Python_Education\Parser\skillbox.html', 'r', encoding='utf-8')
-code = htmlfile.read()
-soup = BeautifulSoup(code, "html.parser")
-links = soup.findAll('a')
-#print(*[link.string.strip() for link in links]) #вывод меню
-print(*[link['href'] for link in links]) #вывод ссылок
+# htmlfile= open('Python_Education\Parser\skillbox.html', 'r', encoding='utf-8')
+# code = htmlfile.read()
+# soup = BeautifulSoup(code, "html.parser")
+# links = soup.findAll('a')
+# #print(*[link.string.strip() for link in links]) #вывод меню
+# print(*[link['href'] for link in links]) #вывод ссылок
 
 kuf = requests.get("https://kuf.aero/board/?type=arr")
-print(kuf.status_code)
+print(kuf)
 #board__text-extra
+
 kufaero = BeautifulSoup(kuf.content, "html.parser")
-kuf_iata = kufaero.findAll('board__text-extra')
-print(*kuf_iata)
+kuf_table = kufaero.find_all(class_ = "table-flex__row")
+result = []
+for board in kuf_table:
+    middle_res = ''
+    arrival_board = board.find(class_ = "table-flex__td table-flex__td--type2")
+    if arrival_board:
+       middle_res += str(arrival_board).split()[3][6:].split('<')[0] + ' '
+        # print(arrival_board.attrs['table-flex__td'])
+        # middle_res += str(arrival_board) + ' '
+    date = board.find(class_ = "table-flex__td table-flex__td--type6") 
+    if date:
+        middle_res += date.find(class_ = "board__text-extra").string.strip() + ' '
+        middle_res += date.find(class_ = "board__text").string.strip() + ' '
+    where_board = board.find(class_ = "table-flex__td table-flex__td--type4")
+    if where_board:
+        middle_res += where_board.find(class_ = "board__text-extra").string.strip() + ' '
+        middle_res += where_board.find(class_ = "board__text").string.strip() + ' '
+    status_board = board.find(class_ = "table-flex__td table-flex__td--type5")
+    if status_board:
+        middle_res += where_board.find(class_ = "board__text-extra").string.strip()
+    if middle_res != '':
+        result.append(middle_res.split())
+    
+print(result)
